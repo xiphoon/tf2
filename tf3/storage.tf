e7cc63307ef5863a6a9f2eaebb9746d021fe93eb
xiphoon <sshpak707@gmail.com>
2026-02-19 20:15:10 +0200

    Update storage.tf

--- tf3/storage.tf
+++ tf3/storage.tf
@@ -1,42 +1,21 @@
-############################################
-# Variables
-############################################
-
-variable "aws_region" {
-  description = "AWS region where all resources will be created."
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Globally unique name for the S3 bucket used to store project data."
-  type        = string
-  default     = "cmtr-0iy36mkm-bucket-1771524368"
-}
-
-############################################
-# Provider
-############################################
-
 provider "aws" {
   region = var.aws_region
 }
 
-############################################
-# S3 Bucket
-############################################
+locals {
+  bucket_name = "${var.project}-bucket-${var.bucket_suffix}"
+}
 
-resource "aws_s3_bucket" "cmtr_bucket" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "this" {
+  bucket = local.bucket_name
 
   tags = {
-    Project = "cmtr-0iy36mkm"
+    Project = var.project
   }
 }
 
-# Block public access on the bucket (recommended best practice)
-resource "aws_s3_bucket_public_access_block" "cmtr_bucket_block" {
-  bucket = aws_s3_bucket.cmtr_bucket.id
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
   block_public_policy     = true
