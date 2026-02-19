5d6cb09e86457ddf102fca08a55b7628660f6dff
xiphoon <sshpak707@gmail.com>
2026-02-19 21:00:21 +0200

    Update variables.tf

--- tf5/variables.tf
+++ tf5/variables.tf
@@ -1,15 +1,34 @@
-variable "aws_region" { description = "AWS region to operate in" type = string }
+variable "project_id" {
+  description = "Project identifier used for tags and resource names (e.g. cmtr-0iy36mkm). Provide this in terraform.tfvars."
+  type        = string
+}
 
-variable "project_id" { description = "Project identifier used for naming and tagging" type = string }
+variable "allowed_ip_range" {
+  description = "List of CIDR blocks allowed to access security group ingress (e.g. ['18.153.146.156/32','203.0.113.25/32']). Provide this in terraform.tfvars."
+  type        = list(string)
+}
 
-variable "allowed_ip_range" { description = "List of CIDR ranges allowed to access public endpoints" type = list(string) }
+variable "vpc_id" {
+  description = "ID of the existing VPC (provided by the platform)."
+  type        = string
+}
 
-variable "vpc_id" { description = "ID of the VPC where security groups will be created" type = string }
+variable "public_subnet_id" {
+  description = "ID of the pre-created public subnet (provided by the platform)."
+  type        = string
+}
 
-variable "public_subnet_id" { description = "ID of the public subnet" type = string }
+variable "private_subnet_id" {
+  description = "ID of the pre-created private subnet (provided by the platform)."
+  type        = string
+}
 
-variable "private_subnet_id" { description = "ID of the private subnet" type = string }
+variable "public_instance_id" {
+  description = "ID of the existing public EC2 instance (provided by the platform)."
+  type        = string
+}
 
-variable "public_instance_id" { description = "EC2 instance ID for the public instance" type = string }
-
-variable "private_instance_id" { description = "EC2 instance ID for the private instance" type = string }
+variable "private_instance_id" {
+  description = "ID of the existing private EC2 instance (provided by the platform)."
+  type        = string
+}
