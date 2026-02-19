dccede7e31de1872392df284296f34d90fdec0f4
xiphoon <sshpak707@gmail.com>
2026-02-19 21:00:13 +0200

    Update versions.tf

--- tf5/versions.tf
+++ tf5/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 4.0"
     }
   }
 }
 
 provider "aws" {
-  region = var.aws_region
+  region = "us-east-1"
 }
