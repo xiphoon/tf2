8bd8f75866272b3e82bd94ead95ab326f174ce5b
xiphoon <sshpak707@gmail.com>
2026-02-19 20:53:19 +0200

    Update outputs.tf

--- tf5/outputs.tf
+++ tf5/outputs.tf
@@ -1,24 +1,24 @@
 output "ssh_security_group_id" {
-  description = "ID of the SSH security group"
+  description = "ID of SSH security group"
   value       = aws_security_group.ssh.id
 }
 
 output "public_http_security_group_id" {
-  description = "ID of the public HTTP security group"
+  description = "ID of public HTTP security group"
   value       = aws_security_group.public_http.id
 }
 
 output "private_http_security_group_id" {
-  description = "ID of the private HTTP security group"
+  description = "ID of private HTTP security group"
   value       = aws_security_group.private_http.id
 }
 
-output "public_instance_primary_nic" {
-  description = "Primary network interface id of the public instance"
-  value       = data.aws_instance.public_instance.primary_network_interface_id
+output "public_instance_network_interface_id" {
+  description = "Network interface ID of public instance"
+  value       = data.aws_instance.public_instance.network_interface_id
 }
 
-output "private_instance_primary_nic" {
-  description = "Primary network interface id of the private instance"
-  value       = data.aws_instance.private_instance.primary_network_interface_id
+output "private_instance_network_interface_id" {
+  description = "Network interface ID of private instance"
+  value       = data.aws_instance.private_instance.network_interface_id
 }
