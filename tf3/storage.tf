20a4b3a8f26e3fe492161b33e70e1f7fcac150d9
xiphoon <sshpak707@gmail.com>
2026-02-19 20:12:58 +0200

    Update storage.tf

--- tf3/storage.tf
+++ tf3/storage.tf
@@ -1,22 +1,40 @@
-provider "aws" {
-  region = "us-east-1"
+############################################
+# Variables
+############################################
+
+variable "aws_region" {
+  description = "AWS region where all resources will be created."
+  type        = string
+  default     = "us-east-1"
 }
 
-# S3 bucket for project cmtr-0iy36mkm
-resource "aws_s3_bucket" "cmtr_bucket" {
-  bucket = "cmtr-0iy36mkm-bucket-1771524368"
-  acl    = "private"
+variable "bucket_name" {
+  description = "Globally unique name for the S3 bucket used to store project data."
+  type        = string
+  default     = "cmtr-0iy36mkm-bucket-1771524368"
+}
 
-  versioning {
-    enabled = true
-  }
+############################################
+# Provider
+############################################
+
+provider "aws" {
+  region = var.aws_region
+}
+
+############################################
+# S3 Bucket
+############################################
+
+resource "aws_s3_bucket" "cmtr_bucket" {
+  bucket = var.bucket_name
 
   tags = {
     Project = "cmtr-0iy36mkm"
   }
 }
 
-# Block public access on the bucket (recommended)
+# Block public access on the bucket (recommended best practice)
 resource "aws_s3_bucket_public_access_block" "cmtr_bucket_block" {
   bucket = aws_s3_bucket.cmtr_bucket.id
 
