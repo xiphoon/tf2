88624d71b67b4e391d00db2fa772cf4b67022153
xiphoon <sshpak707@gmail.com>
2026-02-20 22:12:27 +0200

    Update variables.tf

--- tf7/variables.tf
+++ tf7/variables.tf
@@ -1,1 +1,19 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+}
 
+variable "project_id" {
+  description = "Project identifier for tagging (e.g. cmtr-0iy36mkm)"
+  type        = string
+}
+
+variable "state_bucket" {
+  description = "S3 bucket name that stores the remote Terraform state"
+  type        = string
+}
+
+variable "state_key" {
+  description = "S3 key (path) to the remote Terraform state file"
+  type        = string
+}
