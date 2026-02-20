fc336041672e16c49803293451ce9acee8f20f52
xiphoon <sshpak707@gmail.com>
2026-02-20 22:29:27 +0200

    Create versions.tf

---
+++ tf8/versions.tf
@@ -0,0 +1,14 @@
+###################################################
+# versions.tf
+###################################################
+
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
