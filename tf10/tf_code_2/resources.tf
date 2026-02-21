bc9cfe9ff441dea6e17af3a650f71b8a58f4dc23
xiphoon <sshpak707@gmail.com>
2026-02-21 18:41:45 +0200

    Update resources.tf

--- tf10/tf_code_2/resources.tf
+++ tf10/tf_code_2/resources.tf
@@ -1,19 +1,19 @@
 resource "aws_iam_policy" "custom_policy" {
   name        = local.policy_name
   description = var.policy_description
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "AllowS3List"
-        Effect = "Allow"
-        Action = [
-          "s3:ListBucket",
-        ]
-        Resource = ["*"]
-      }
-    ]
-  })
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 
   tags = local.common_tags
 }
