43141817d4fdb6124d9964a05578ea3716a46e7e
xiphoon <sshpak707@gmail.com>
2026-02-21 17:43:28 +0200

    Create custom_policy.tf

---
+++ tf10/tf_code_2/iam/custom_policy.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_policy" "custom_policy" {
+  name        = local.policy_name
+  description = var.policy_description
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AllowS3List"
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+        ]
+        Resource = ["*"]
+      }
+    ]
+  })
+
+  tags = local.common_tags
+}
