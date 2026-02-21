f75b277045aec2484967de7a77122af287f68630
xiphoon <sshpak707@gmail.com>
2026-02-21 18:11:09 +0200

    Update outputs.tf

--- tf10/tf_code_2/outputs.tf
+++ tf10/tf_code_2/outputs.tf
@@ -1,5 +1,1 @@
-output "custom_policy_arn" {
-  description = "ARN of the custom IAM policy moved into tf_code_2"
-  value       = aws_iam_policy.custom_policy.arn
-  depends_on  = [aws_iam_policy.custom_policy]
-}
+
