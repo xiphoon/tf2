83ed23049f18cfb2fb8ee553152be023cdcd1d08
xiphoon <sshpak707@gmail.com>
2026-02-22 21:52:52 +0200

    Create locals.tf

---
+++ tf13/locals.tf
@@ -0,0 +1,15 @@
+locals {
+  common_tags = {
+    ManagedBy = "terraform"
+    Project   = "blue-green-lab"
+    Prefix    = var.name_prefix
+  }
+
+  alb_name         = "${var.name_prefix}-lb"
+  blue_tg_name     = "${var.name_prefix}-blue-tg"
+  green_tg_name    = "${var.name_prefix}-green-tg"
+  blue_lt_name     = "${var.name_prefix}-blue-template"
+  green_lt_name    = "${var.name_prefix}-green-template"
+  blue_asg_name    = "${var.name_prefix}-blue-asg"
+  green_asg_name   = "${var.name_prefix}-green-asg"
+}
