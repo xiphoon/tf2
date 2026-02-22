82e229909fb0509c54b89e34dde07e9989f71374
xiphoon <sshpak707@gmail.com>
2026-02-22 22:15:38 +0200

    Update alb.tf

--- tf13/alb.tf
+++ tf13/alb.tf
@@ -34,4 +34,34 @@ resource "aws_lb_target_group" "green" {
 
   health_check {
     path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = merge(local.common_tags, { Name = local.green_tg_name })
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = var.blue_weight
+      }
+
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = var.green_weight
+      }
+    }
+  }
 }
