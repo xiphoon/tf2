9d9a3dd8a6199ae0ad014b8e7c38b9678da25e13
xiphoon <sshpak707@gmail.com>
2026-02-22 21:59:16 +0200

    Update asg.tf

--- tf13/asg.tf
+++ tf13/asg.tf
@@ -1,15 +1,13 @@
 resource "aws_autoscaling_group" "blue" {
-  name                      = local.blue_asg_name
-  desired_capacity          = var.desired_capacity
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  vpc_zone_identifier       = var.public_subnet_ids
-  health_check_type         = "ELB"
-  health_check_grace_period = 300
+  name                = local.blue_asg_name
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.public_subnet_ids
 
   launch_template {
     id      = aws_launch_template.blue.id
-    version = "\$Latest"
+    version = "$Latest"
   }
 
   target_group_arns = [aws_lb_target_group.blue.arn]
@@ -19,26 +17,18 @@ resource "aws_autoscaling_group" "blue" {
     value               = local.blue_asg_name
     propagate_at_launch = true
   }
-
-  tags = [for k, v in local.common_tags : {
-    key                 = k
-    value               = v
-    propagate_at_launch = true
-  }]
 }
 
 resource "aws_autoscaling_group" "green" {
-  name                      = local.green_asg_name
-  desired_capacity          = var.desired_capacity
-  max_size                  = var.max_size
-  min_size                  = var.min_size
-  vpc_zone_identifier       = var.public_subnet_ids
-  health_check_type         = "ELB"
-  health_check_grace_period = 300
+  name                = local.green_asg_name
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
+  vpc_zone_identifier = var.public_subnet_ids
 
   launch_template {
     id      = aws_launch_template.green.id
-    version = "\$Latest"
+    version = "$Latest"
   }
 
   target_group_arns = [aws_lb_target_group.green.arn]
@@ -48,10 +38,4 @@ resource "aws_autoscaling_group" "green" {
     value               = local.green_asg_name
     propagate_at_launch = true
   }
-
-  tags = [for k, v in local.common_tags : {
-    key                 = k
-    value               = v
-    propagate_at_launch = true
-  }]
 }
