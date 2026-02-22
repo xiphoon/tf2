7e1df2c2b60bdd37e1cbb73f95a32e9843f11a52
xiphoon <sshpak707@gmail.com>
2026-02-22 21:59:29 +0200

    Update outputs.tf

--- tf13/outputs.tf
+++ tf13/outputs.tf
@@ -3,22 +3,12 @@ output "alb_dns_name" {
   value       = aws_lb.alb.dns_name
 }
 
-output "blue_target_group_arn" {
-  description = "ARN of the Blue target group."
-  value       = aws_lb_target_group.blue.arn
-}
-
-output "green_target_group_arn" {
-  description = "ARN of the Green target group."
-  value       = aws_lb_target_group.green.arn
-}
-
 output "blue_asg_name" {
-  description = "Name of the Blue Auto Scaling Group."
+  description = "Name of the Blue ASG."
   value       = aws_autoscaling_group.blue.name
 }
 
 output "green_asg_name" {
-  description = "Name of the Green Auto Scaling Group."
+  description = "Name of the Green ASG."
   value       = aws_autoscaling_group.green.name
 }
