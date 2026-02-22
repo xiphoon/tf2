f54b97d35f2f895e3a8400c9dd61e7a32bedb34d
xiphoon <sshpak707@gmail.com>
2026-02-22 21:58:45 +0200

    Update locals.tf

--- tf13/locals.tf
+++ tf13/locals.tf
@@ -5,11 +5,11 @@ locals {
     Prefix    = var.name_prefix
   }
 
-  alb_name         = "${var.name_prefix}-lb"
-  blue_tg_name     = "${var.name_prefix}-blue-tg"
-  green_tg_name    = "${var.name_prefix}-green-tg"
-  blue_lt_name     = "${var.name_prefix}-blue-template"
-  green_lt_name    = "${var.name_prefix}-green-template"
-  blue_asg_name    = "${var.name_prefix}-blue-asg"
-  green_asg_name   = "${var.name_prefix}-green-asg"
+  alb_name       = "${var.name_prefix}-lb"
+  blue_tg_name   = "${var.name_prefix}-blue-tg"
+  green_tg_name  = "${var.name_prefix}-green-tg"
+  blue_lt_name   = "${var.name_prefix}-blue-template"
+  green_lt_name  = "${var.name_prefix}-green-template"
+  blue_asg_name  = "${var.name_prefix}-blue-asg"
+  green_asg_name = "${var.name_prefix}-green-asg"
 }
