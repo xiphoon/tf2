fb8677e5af47b0e1905927756366df36f2185482
xiphoon <sshpak707@gmail.com>
2026-02-23 00:10:16 +0200

    Update launch_templates.tf

--- tf13/launch_templates.tf
+++ tf13/launch_templates.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "blue" {
   image_id      = var.ami_id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = compact([var.sg_http_id, var.sg_ssh_id])
+  vpc_security_group_ids = [var.sg_http_id, var.sg_ssh_id]
   key_name               = length(var.key_name) > 0 ? var.key_name : null
 
   user_data = base64encode(<<EOF
@@ -29,39 +29,5 @@ EOF
   tag_specifications {
     resource_type = "instance"
     tags          = merge(local.common_tags, { Name = local.blue_lt_name })
-  }
-}
-
-resource "aws_launch_template" "green" {
-  name_prefix   = local.green_lt_name
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-
-  vpc_security_group_ids = compact([var.sg_http_id, var.sg_ssh_id])
-  key_name               = length(var.key_name) > 0 ? var.key_name : null
-
-  user_data = base64encode(<<EOF
-#!/bin/bash
-set -e
-
-if command -v yum >/dev/null 2>&1; then
-  yum update -y
-  yum install -y httpd
-  systemctl enable httpd
-  echo "<html><body><h1>Green Environment</h1></body></html>" > /var/www/html/index.html
-  systemctl start httpd
-elif command -v apt-get >/dev/null 2>&1; then
-  apt-get update -y
-  apt-get install -y apache2
-  systemctl enable apache2
-  echo "<html><body><h1>Green Environment</h1></body></html>" > /var/www/html/index.html
-  systemctl start apache2
-fi
-EOF
-  )
-
-  tag_specifications {
-    resource_type = "instance"
-    tags          = merge(local.common_tags, { Name = local.green_lt_name })
   }
 }
