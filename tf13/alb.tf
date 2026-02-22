617c752c9a4ab06a8d601b3a6e9ebbd2e0611b95
xiphoon <sshpak707@gmail.com>
2026-02-22 21:53:10 +0200

    Create alb.tf

---
+++ tf13/alb.tf
@@ -0,0 +1,67 @@
+resource "aws_lb" "alb" {
+  name               = local.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.public_subnet_ids
+  security_groups    = [var.sg_lb_id]
+
+  tags = merge(local.common_tags, { Name = local.alb_name })
+}
+
+resource "aws_lb_target_group" "blue" {
+  name     = local.blue_tg_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = merge(local.common_tags, { Name = local.blue_tg_name })
+}
+
+resource "aws_lb_target_group" "green" {
+  name     = local.green_tg_name
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    matcher             = "200-399"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = merge(local.common_tags, { Name = local.green_tg_name })
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        target_group_arn = aws_lb_target_group.blue.arn
+        weight           = var.blue_weight
+      }
+
+      target_group {
+        target_group_arn = aws_lb_target_group.green.arn
+        weight           = var.green_weight
+      }
+    }
+  }
+}
