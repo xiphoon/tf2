97694249e28d26d78c195be2368e1495cf405a02
xiphoon <sshpak707@gmail.com>
2026-02-23 18:57:37 +0200

    Update variables.tf

--- tf14/modules/network_security/variables.tf
+++ tf14/modules/network_security/variables.tf
@@ -1,1 +1,14 @@
+variable "project_prefix" {
+  description = "Prefix used to name resources."
+  type        = string
+}
 
+variable "vpc_id" {
+  description = "VPC ID where security groups will be created."
+  type        = string
+}
+
+variable "allowed_ip" {
+  description = "List of CIDR ranges allowed to access SSH/HTTP (e.g. [\"18.153.146.156/32\",\"203.0.113.25/32\"])."
+  type        = list(string)
+}
