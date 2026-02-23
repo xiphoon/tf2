bc97be7753b759a8d1ab6fd2f89d691aa179fd73
xiphoon <sshpak707@gmail.com>
2026-02-23 19:26:21 +0200

    Update main.tf

--- tf14/modules/application/main.tf
+++ tf14/modules/application/main.tf
@@ -125,5 +125,5 @@ resource "aws_autoscaling_group" "app" {
 
 resource "aws_autoscaling_attachment" "asg_tg" {
   autoscaling_group_name = aws_autoscaling_group.app.name
-  target_group_arn       = aws_lb_target_group.app.arn
+  alb_target_group_arn   = aws_lb_target_group.app.arn
 }
